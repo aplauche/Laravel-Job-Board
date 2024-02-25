aac676634d851a75c6dc67c041a2c415cfdcab9a
Anton Plauche <antonplauche92@gmail.com>
2024-02-25 16:28:53 -0600

    job listing view for employers

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -1,5 +1,45 @@
 <x-layout>
-  <x-card>
-    All Jobs
-  </x-card>
+  <x-breadcrumbs class="mb-4" :links="[
+    'My Jobs' => '#'
+  ]" />
+
+  <div class="mb-8 text-right">
+    <x-button-link href="{{route('my-jobs.create')}}">Create Job Listing</x-button-link>
+  </div>
+
+  @forelse ($jobs as $job)
+
+  <x-job-card :job="$job">
+   <div class="text-xs text-slate-500">
+      @forelse ($job->jobApplications as $application)
+          <div class="mb-4 flex items-center justify-between">
+            <div>
+              <div><strong>{{ $application->user->name }}</strong></div>
+              <div>Applied {{$application->created_at->diffForHumans()}}</div>
+              <div>Download CV</div>
+            </div>
+            <div>
+              ${{number_format($application->expected_salary)}}
+            </div>
+          </div>
+      @empty
+          <div>
+            No applications yet...
+          </div>
+      @endforelse
+   </div>
+  </x-job-card>
+      
+  @empty
+      <div class="rounded-md border border-dashed border-slate-300 p-8">
+        <div class="text-center font-medium">
+          No jobs yet
+        </div>
+        <div class="text-center">
+          Post your first job <a class="text-indigo-500 hover:underline" href="{{route('my-jobs.create')}}">here!</a>
+        </div>
+      </div>
+  @endforelse
+
+
 </x-layout>
